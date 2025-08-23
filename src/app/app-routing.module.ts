a5c68c8c7f2ae28de3bfa00ab97413305a9cd799
arun <aruntechgeek15@gmail.com>
2025-08-23 21:07:04 +0530

    created dashboard page for seller

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './seller/dashboard/dashboard.component';
+import { OrdersComponent } from './seller/orders/orders.component';
+import { ProdutcsComponent } from './seller/produtcs/produtcs.component';
+import { MarketingComponent } from './seller/marketing/marketing.component';
+import { AnalyticsComponent } from './seller/analytics/analytics.component';
 
 const routes: Routes = [
   {path:"login",component:LoginComponent},
   {path:"signup",component:SignupComponent},
   {path:"home",component:HomeComponent},
-  {path:"",redirectTo:"/signup",pathMatch:"full"}
+  {path:"",redirectTo:"signup",pathMatch:"full",},
+  {path:"seller",component:DashboardComponent},
+  {path:"seller/orders",component:OrdersComponent},
+  {path:"seller/products",component:ProdutcsComponent},
+  {path:"seller/marketing",component:MarketingComponent},
+  {path:"seller/analytics",component:AnalyticsComponent},
+  {path:"seller/dashboard",redirectTo:"seller",pathMatch:"full"}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,25 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { CustomersComponent } from './seller/customers/customers.component';
+import { MarketingComponent } from './seller/marketing/marketing.component';
+import { ProdutcsComponent } from './seller/produtcs/produtcs.component';
+import { OrdersComponent } from './seller/orders/orders.component';
+import { AnalyticsComponent } from './seller/analytics/analytics.component';
+import { DashboardComponent } from './seller/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    CustomersComponent,
+    MarketingComponent,
+    ProdutcsComponent,
+    OrdersComponent,
+    AnalyticsComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
